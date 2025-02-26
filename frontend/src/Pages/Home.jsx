d49df39d4949965b480c0624fdf2af65dd326670
Yakshit Wadhwa <yakshitwadhwa29940@gmail.com>
2025-02-26 19:29:09 +0530

    first commit

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Hero from '../Components/Hero'
-import LatestCollection from '../Components/latestcollection'
-import BestSeller from '../Components/BestSeller'
-import OurPolicy from '../Components/OurPolicy'
-import NewsLetter from '../Components/NewsLetter'
+import Hero from '../Components/Hero.jsx'
+import LatestCollection from '../Components/latestcollection.jsx'
+import BestSeller from '../Components/BestSeller.jsx'
+import OurPolicy from '../Components/OurPolicy.jsx'
+import NewsLetter from '../Components/NewsLetter.jsx'
 const Home = () => {
   return (
     <div>
